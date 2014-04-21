e3399d95efb01f33355ec831064d6aff124a7e78
epequeno <estevan.pequeno@gmail.com>
2014-04-20 21:39:44 -0500

    first crack at 1D

---
+++ 1D/1d.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 20:48:08 2014
+
+@author: steven
+"""
+
+data = [line.strip("\n") for line in open("rosalind_1d.txt", "r")]
+
+genome = data[0]
+values = data[1].split()
+k = int(values[0])
+L = int(values[1])
+t = int(values[2])
+
+def makeCandidates(genome):
+    candidates = []
+    for i in range(0, len(genome)):
+        candidate = genome[i: i + k]
+        if candidate in candidates or len(candidate) < k:
+            pass
+        else:
+            candidates.append(candidate)
+    return candidates
+
+def makeWindows(genome):
+    windows = []
+    for i in range(0, len(genome)):
+        window = genome[i: i + L]
+        if window in windows or len(window) < L:
+            pass
+        else:
+            windows.append(window)
+    return windows
+
+def matches(candidate, window):
+    count = 0
+    for i in range(0, len(window)):
+        if window[i: i + k] == candidate:
+            count += 1
+    return count
+
+    
+candidates = makeCandidates(genome)
+windows = makeWindows(genome)
+
+print k, L, t
+print len(candidates)
+print len(windows)
+
+
+
