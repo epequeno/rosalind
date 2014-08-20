67278af7f9f39aafa1e8c9d6742b65e816df234b
Estevan Pequeno <estevan.pequeno@gmail.com>
2014-08-20 18:50:20 -0500

    oh fasta

--- IPRB/iprb.py
+++ IPRB/iprb.py
@@ -9,17 +9,7 @@ import random
 
 dataset = [int(i) for i in open('rosalind_iprb.txt', 'r').read().split()]
 
-total = sum(dataset)
-dominant_probibility = dataset[0] / float(total)
-hetero_probibility = dataset[1] / float(total)
-recessive_probibility = dataset[2] / float(total)
-
-population = [dominant_probibility, hetero_probibility, recessive_probibility]
-
-male = random.choice(population)
-female = random.choice(population)
-
-# Punnet probibilities
+# Punnet
 dom_dom = 1.0
 dom_het = 1.0
 dom_rec = 1.0
@@ -27,4 +17,32 @@ het_rec = 0.5
 het_het = 0.75
 rec_rec = 0.0
 
+population = []
 
+population.extend(['dom'] * dataset[0])
+population.extend(['het'] * dataset[1])
+population.extend(['rec'] * dataset[2])
+
+
+def chance_of_dominant():
+    male = random.choice(population)
+    female = random.choice(population)
+    if male == 'dom' or female == 'dom':
+        return dom_dom
+    elif male == 'het' and female == 'rec':
+        return het_rec
+    elif male == 'rec' and female == 'het':
+        return het_rec
+    elif male == 'het' and female == 'het':
+        return het_het
+    else:
+        return rec_rec
+
+
+def group_chances(n):
+    results = 0
+    for i in range(n):
+        results *= chance_of_dominant()
+    return results
+
+print group_chances(100)
